f00c7837d381d5ba8c865317fc7fa8cf720f6ad7
Carlos Roig <carroisal3@gmail.com>
2016-11-23 19:36:02 +0100

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Carlos Roig on 23/11/16.
+//  Copyright © 2016 minube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
