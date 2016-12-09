e14a84d5592304678ea409355e62ce469393e2f6
Carlos <carroisal3@gmail.com>
2016-12-09 12:01:44 +0100

    Removed pods

--- PhotoGallery/PhotoGallery/PhotoCellViewModel.swift
+++ PhotoGallery/PhotoGallery/PhotoCellViewModel.swift
@@ -12,21 +12,14 @@ import SDWebImage
 struct PhotoCellViewModel : ImagePresentable {
     var imageURL: URL?
     var imageName : String?
-    
     var placeholder: String? {return ""}
     
-    func image(_ size: CGSize, completion: @escaping (UIImage?) -> ()) {
-        guard let imageName = imageName else {
-            completion(UIImage(named:placeholder!))
-            return
-        }
-        completion(UIImage(named:imageName))
-    }
-}
+   }
 
 protocol ImagePresentable {
     var placeholder: String? {get}
     var imageURL: URL? {get}
+    var imageName : String? {get}
     var contentMode: UIViewContentMode {get}
     func image (_ size: CGSize,completion: @escaping (UIImage?)->())
 }
@@ -35,13 +28,12 @@ extension ImagePresentable {
     var placeholder: String? {return nil}
     var contentMode: UIViewContentMode {return .scaleAspectFit}
     
-    func image (_ size: CGSize,completion: @escaping (UIImage?)->()){
-        SDWebImageManager.shared().downloadImage(with: self.imageURL, options:.retryFailed, progress: nil) { (image, error, cacheType, finished, imageURL) in
-            if image == nil {
-                completion(UIImage(named:"ic_noimage"))
-            } else {
-                completion(image)
-            }
+    func image(_ size: CGSize, completion: @escaping (UIImage?) -> ()) {
+        guard let imageName = imageName else {
+            completion(UIImage(named:placeholder!))
+            return
         }
+        completion(UIImage(named:imageName))
     }
+
 }
